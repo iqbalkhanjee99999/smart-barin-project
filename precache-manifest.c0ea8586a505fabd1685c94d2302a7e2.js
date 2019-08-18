37fac3bc786df3e106973d08f71062720d74aec4
iqbal khan <iqbalhussainkhan@yahoo.com>
2019-08-18 09:53:09 +0400

    Updates

---
+++ precache-manifest.c0ea8586a505fabd1685c94d2302a7e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226ac565bf8b88e6712f92d3a7e05897",
+    "url": "/smart-barin-project/index.html"
+  },
+  {
+    "revision": "7c28b66c78361d0b5852",
+    "url": "/smart-barin-project/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b0f301aadd17571ef9dc",
+    "url": "/smart-barin-project/static/css/main.703cea33.chunk.css"
+  },
+  {
+    "revision": "7c28b66c78361d0b5852",
+    "url": "/smart-barin-project/static/js/2.15361e12.chunk.js"
+  },
+  {
+    "revision": "b0f301aadd17571ef9dc",
+    "url": "/smart-barin-project/static/js/main.bd450be6.chunk.js"
+  },
+  {
+    "revision": "5099414bd3118c7e0ff5",
+    "url": "/smart-barin-project/static/js/runtime~main.21db1787.js"
+  }
+]);
