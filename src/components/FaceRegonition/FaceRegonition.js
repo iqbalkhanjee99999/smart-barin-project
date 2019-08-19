004e55ad9bf812cd98c6bf7d2ae5ab798d67af15
iqbal khan <iqbalhussainkhan@yahoo.com>
2019-08-19 17:09:52 +0400

    face replace with emoji

--- src/components/FaceRegonition/FaceRegonition.js
+++ src/components/FaceRegonition/FaceRegonition.js
@@ -7,7 +7,10 @@ const FaceRegonition = ({ image,boxes,error }) =>{
 
 	if(boxes && boxes.length > 0){
 		 faceBoxes = boxes.map(box => {
-			return <div className='bounding-box' style={{top: box.topRow, bottom: box.bottomRow,'left': box.leftCol,right: box.rightCol}}></div>
+			return(
+				 <div className='bounding-box' style={{top: box.topRow , bottom: box.bottomRow ,left: box.leftCol - 10,right: box.rightCol - 10,}}>
+				</div>
+			)
 		})
 	}
 
